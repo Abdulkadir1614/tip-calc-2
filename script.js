8bc1e9c0b1466b704d2cc1708356a5c2f18af300
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-07 17:18:09 +0800

    DONE

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("hello");
+let bill = 275;
+let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+let total = bill + tip ;
+console.log(`The bill was ${bill}, and the tip was ${tip} and the Total value ${total}`)
